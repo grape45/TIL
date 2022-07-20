c803f0bc15b24e16551d25dcf862eaddf1af63d5
GJ <gwangjin392@gmail.com>
2022-07-20 10:56:22 +0900

    0720 modified

---
+++ class_solving/0720/00_oop.py
@@ -0,0 +1,10 @@
+# 클래스 정의
+class Person:
+    pass
+
+# 인스턴스 생성
+p1 = Person()
+# 인스턴스 속성
+p1.name = '홍길동'
+
+print(p1.name)
--- class_solving/0720/05_user.py
+++ class_solving/0720/05_user.py
@@ -4,4 +4,4 @@ import lotto_function
 # 로또 번호들의 리스트
 buy_numbers = lotto_function.generate_lotto(5)
 
-lotto_function.check(buy_number, [1, 2, 3, 4, 5, 6])
+lotto_function.check(buy_numbers, [1, 2, 3, 4, 5, 6])
