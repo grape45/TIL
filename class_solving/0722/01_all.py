69ac14b071a465d6a5b8a4e90d18bbad5b070c55
GJ <gwangjin392@gmail.com>
2022-07-25 09:13:11 +0900

    0725 commit

--- class_solving/0722/01_all.py
+++ class_solving/0722/01_all.py
@@ -7,10 +7,16 @@ coins = response.get('data')
 
 # coins : 딕셔너리
 # key : coin 이름
-# value : 딕셔너리(코인의 정보)
+# value : 딕셔너리(코인의 정보) <-
 for coin in coins:
-    # coins.get(coin) <- 코인의 정봉니 딕셔너리
+    # coins.get(coin) <- 코인의 정보인 딕셔너리
     # 그 딕셔너리의 closing price
-    if coin == 'data':
+    if coin == 'date':
         continue
-    print(coin, coins.get(coin).get('closing_price')) ------- modified code
+    print(coin, coins.get(coin).get('closing_price')) 
+
+
+# AttributeError: 'str' object has no attribute 'get'
+# 사용한 데이터 어딘가에  'str' object가 있어서 발생한 오류
+# coins 가 문자열이었거나 coins.get(coin)이 문자열이었거나 둘 중 하나 
+ 
