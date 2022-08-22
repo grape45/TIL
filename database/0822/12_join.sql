01aecfb973a69db84f2677450b78f5d1386794dc
Gwang Jin <gwangjin392@gmail.com>
2022-08-22 13:11:21 +0900

    0822_upload

--- database/0822/12_join.sql
+++ database/0822/12_join.sql
@@ -21,3 +21,6 @@ SELECT * FROM articles FULL OUTER JOIN users ON articles.user_id = users.id;
 
 -- CROSS JOIN
 SELECT * FROM users CROSS JOIN role;
+
+-- 3개의 테이블 조인
+SELECT * FROM articles JOIN users ON articles.user_id = users.id JOIN role ON users.role_id = role.id;
